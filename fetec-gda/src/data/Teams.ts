5fa834fe583ced98f0a886aa0a05589bea0c4d47
Vicente Viera <plantedcoot706@gmail.com>
2021-12-09 19:58:10 -0600

    Team bubble2

---
+++ fetec-gda/src/data/Teams.ts
@@ -0,0 +1,15 @@
+const Teams = [
+    {img:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRQFkEnG-lDOcZHEsEHa3IlcOU_J2pgjUM1ZaVyYZUxj5em-1OIAZuKj2Ox74G4u7s8bBA&usqp=CAU',
+     name:"bodoke",
+     role:"admin de discord",
+     brand:"Brand0"   
+    },
+    {img:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRQFkEnG-lDOcZHEsEHa3IlcOU_J2pgjUM1ZaVyYZUxj5em-1OIAZuKj2Ox74G4u7s8bBA&usqp=CAU',
+    name:"xD",
+    role:"admin de discord",
+    brand:"Brand1"   
+   },
+  ];
+  
+  export default Teams;
+  
